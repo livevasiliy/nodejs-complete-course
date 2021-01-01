8e8cbcd4f657c6c80297a8783cfd433d50b7d072
Vasiliy Pivovarov <livevasiliy@yandex.ru>
2021-01-01 10:42:15 +0300

    finish 8 module

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-const notFoundController = require('./controllers/not-found');
+const notFoundController = require('./controllers/error');
 
 app.use(bodyParser.urlencoded({
   extended: false
@@ -21,6 +21,6 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use(notFoundController.returnNotFound);
+app.use(notFoundController.get404);
 
 app.listen(3000);
--- controllers/not-found.js
+++
@@ -1,5 +0,0 @@
-exports.returnNotFound = (req, res, next) => {
-  res.status(404).render('404', {
-    pageTitle: "Page not found"
-  });
-}
