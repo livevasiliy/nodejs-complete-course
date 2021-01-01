3edbfa0a57c0388a581851f08cbf75a75817fc7e
Vasiliy Pivovarov <livevasiliy@yandex.ru>
2021-01-01 07:03:49 +0300

    finish 7 module - MVC pattern

--- app.js
+++ app.js
@@ -8,21 +8,19 @@ const app = express();
 app.set('view engine', 'pug');
 app.set('views', 'views');
 
-const adminData = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+
+const notFoundController = require('./controllers/not-found');
 
 app.use(bodyParser.urlencoded({
   extended: false
 }));
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/admin', adminData.routes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-  res.status(404).render('404', {
-    pageTitle: "Page not found"
-  });
-});
+app.use(notFoundController.returnNotFound);
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -2,18 +2,10 @@ const path = require('path');
 
 const express = require('express');
 
-const rootDir = require('../util/path');
-const adminData = require('./admin');
-
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  const products = adminData.products;
-  res.render('shop', {
-    prods: products,
-    pageTitle: 'Shop',
-    path: '/'
-  });
-});
+const productsController = require('../controllers/products')
+
+router.get('/', productsController.getProducts);
 
 module.exports = router;
